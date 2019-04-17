72e03618030b883b61375ca49f18706d373e6819
Lion Kortlepel <development@kortlepel.com>
2019-04-17 17:01:58 +0200

    Added typedefs

--- Vector2.hpp
+++ Vector2.hpp
@@ -1,9 +1,19 @@
-// File "Vector2.hpp" created April 2019
-// Copyright (c) Lion Kortlepel 2019
+/* File "Vector2.hpp" created April 2019
+ * Copyright (c) Lion Kortlepel 2019
+ * 
+ * This templated class contains all common operations for a 2D Vector.
+ * I decided to document everything so that even someone not quite familiar
+ * with vector maths could confidently use it.
+ */
+
+
+
+
 #ifndef VECTOR2_HPP
 #define VECTOR2_HPP
 
 #include <ostream>
+#include <math.h>
 
 /// Represents a 2D vector with two elements of type T and implements
 /// all common operations associated with it.
@@ -12,7 +22,8 @@ class Vector2
 {
 public:
     /// Vector2 with both elements set to 1.0.
-    static const Vector2 one = Vector2 (1.0, 1.0);
+    static const Vector2 one  = Vector2 (1.0, 1.0);
+    
     /// Vector2 with both elements set to 0.0.
     static const Vector2 zero = Vector2 (0.0, 0.0);
     
@@ -208,4 +219,41 @@ public:
     
 };
 
+
+// common type typedefs
+
+/// float
+typedef Vector2<float>              Vector2f;
+/// double
+typedef Vector2<double>             Vector2d;
+/// char
+typedef Vector2<signed char>        Vector2c;
+/// unsigned char
+typedef Vector2<unsigned char>      Vector2uc;
+/// int
+typedef Vector2<signed int>         Vector2i;
+/// unsigned int
+typedef Vector2<unsigned int>       Vector2u;
+/// long
+typedef Vector2<signed long>        Vector2l;
+/// unsigned long
+typedef Vector2<unsigned long>      Vector2ul;
+/// long long
+typedef Vector2<signed long long>   Vector2ll;
+/// unsigned long long
+typedef Vector2<unsigned long long> Vector2ull;
+
+// int_fast typedefs
+
+typedef Vector2<int_fast8_t>        Vector2i8;
+typedef Vector2<int_fast16_t>       Vector2i16;
+typedef Vector2<int_fast32_t>       Vector2i32;
+typedef Vector2<int_fast64_t>       Vector2i64;
+
+// uint_fast typedefs
+
+typedef Vector2<uint_fast8_t>       Vector2u8;
+typedef Vector2<uint_fast16_t>      Vector2u16;
+typedef Vector2<uint_fast32_t>      Vector2u32;
+typedef Vector2<uint_fast64_t>      Vector2u64;
 #endif //VECTOR2_HPP
